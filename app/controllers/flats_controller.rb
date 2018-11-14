a249608bd5097d2fa8b521cf3db7381a4bf1b3aa
Stefan Wawrzyn <swawrzyn@gmail.com>
2018-11-14 16:29:24 +0800

    added update and delete functionality

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: [:show]
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
   def index
     @flats = Flat.all
   end
@@ -12,6 +12,18 @@ class FlatsController < ApplicationController
 
   def create
     redirect_to flat_path(Flat.create!(flat_params))
+  end
+
+  def edit; end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
   end
 
   private
